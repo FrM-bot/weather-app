237c0b4be39d4030a8e2fe890f7613dcb2758802
Franco M <82059717+FrM-bot@users.noreply.github.com>
2022-05-22 21:42:04 -0300

    Update GET_CITY.js

--- src/services/GET_CITY.js
+++ src/services/GET_CITY.js
@@ -4,14 +4,14 @@ export const GET_CITY = async ({ name, id }) => {
     const apiKey = import.meta.env.VITE_API_KEY
     const queryBy = name ?? id
     const query = name ? 'q' : 'id'
-    console.log({ queryBy, apiKey })
+
     const url = `http://api.openweathermap.org/data/2.5/weather?${query}=${queryBy}&appid=${apiKey}&units=metric`
     const res = await fetch(url)
     const resJSON = await res.json()
-    console.log(res, apiKey)
+
     if (res.status !== 200) return { error: res.statusText }
     return resJSON
   } catch (error) {
-    console.error(error)
+    alert(error)
   }
 }

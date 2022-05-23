8b4fa6bef2e13502d760a63a2b659ef4a57d60b8
Franco M <damianmaciel0@gmail.com>
2022-05-22 21:54:43 -0300

    Console logs eliminados

--- src/containers/City.jsx
+++ src/containers/City.jsx
@@ -2,12 +2,10 @@ import { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import Button from '../components/Button'
 import GoogleMaps from '../components/GoogleMaps'
-// import GoogleMaps from '../components/GoogleMaps'
 import { GET_CITY } from '../services/GET_CITY'
 import './City.css'
 
 const City = ({ city }) => {
-  // const navigate = useNavigate()
   const { id } = useParams()
   const [cityPage, setCityPage] = useState()
   console.log({ id })
@@ -20,7 +18,6 @@ const City = ({ city }) => {
     }
     city && setCityPage(city)
   }, [city])
-  console.log({ cityPage }, { city })
 
   if (!cityPage) return <div className='min-h-50vw center'><div className='spinner' /></div>
   return (
@@ -45,9 +42,6 @@ const City = ({ city }) => {
             <p>Longitud: <span> {cityPage.coord.lon}º</span></p>
           </div>
         </div>
-        {/* <GoogleMaps />
-         */}
-         {/* <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d14244.499454767974!2d-60.82604920000001!3d-26.804152549999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses!2sar!4v1653077526376!5m2!1ses!2sar" width="600" height="450" style="border:0;" allowFullScreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> */}
          <GoogleMaps lat={cityPage.coord.lat} lng={cityPage.coord.lon}/>
       </div>
     </main>
